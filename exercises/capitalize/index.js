45df4352a30f64ff10edac87e1c88f7f6b4699a8
rob <robertchen234@gmail.com>
2019-02-28 18:28:53 -0500

    steps done

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -18,4 +18,18 @@ function capitalize(str) {
   return words.join(" ");
 }
 
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === " ") {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+
+//   return result;
+// }
+
 module.exports = capitalize;
