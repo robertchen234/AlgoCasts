4e93d40b65fb35c06302d047541b31674169c53e
rob <robertchen234@gmail.com>
2019-02-28 18:58:13 -0500

    steps recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,42 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = ''
-
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += '#'
-      } else {
-        stair += ' '
-      }
-    }
-  console.log(stair)
+// recursion
+// 1. think about basecase, when to stop recursion
+// 2. do some work
+// 3. change the argument that will eventually get to basecase
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  steps(n, row, stair);
 }
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = ''
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#'
+//       } else {
+//         stair += ' '
+//       }
+//     }
+//   console.log(stair)
+//   }
+// }
 
 module.exports = steps;
